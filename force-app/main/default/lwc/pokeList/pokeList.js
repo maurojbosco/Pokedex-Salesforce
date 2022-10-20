b8a92f3021f2d848e6b5cabc1af3a82901195639
Mauro Jelvez Bosco <mauro_jelvez@hotmail.com>
2022-10-20 13:50:23 -0300

    version 1.0.3

--- force-app/main/default/lwc/pokeList/pokeList.js
+++ force-app/main/default/lwc/pokeList/pokeList.js
@@ -31,12 +31,8 @@ export default class PokeList extends NavigationMixin(LightningElement) {
 	}
 
 	handleSearchTermChange(event) {
-		// Debouncing this method: do not update the reactive property as
-		// long as this function is being called within a delay of 300 ms.
-		// This is to avoid a very large number of Apex method calls.
 		window.clearTimeout(this.delayTimeout);
 		const searchTerm = event.target.value;
-		// eslint-disable-next-line @lwc/lwc/no-async-operation
 		this.delayTimeout = setTimeout(() => {
 			this.nombre = searchTerm;
 		}, 300);
